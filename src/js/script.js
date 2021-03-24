b16393aad879fe45b66415ecd8b64efba8c90bc2
Olga Kulczycka <olga.kulczycka07@gmail.com>
2021-03-24 10:43:12 +0100

    final

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const border1 = document.getElementById('border1'); 
+const border2 = document.getElementById('border2'); 
+const border3 = document.getElementById('border3'); 
+
+border1.onclick = function() {
+  this.style.borderColor = "black";
+};
+
+border2.onclick = function() {
+  this.style.borderColor = "black";
+};
+
+border3.onclick = function() {
+  this.style.borderColor = "black";
+};
+
+
